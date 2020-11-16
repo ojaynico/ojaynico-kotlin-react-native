f9184cd16e77600fddc2d5ca99b324a1163941a6
ojaynico <ojaynico@gmail.com>
2020-11-17 01:38:44 +0300

    Update gradle for bintray publishing, removed un-necessary sourcesets

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,30 @@
 plugins {
-    kotlin("multiplatform") version "1.4.10"
+    kotlin("js") version "1.4.10"
     `maven-publish`
+    id("com.jfrog.bintray") version "1.8.4"
 }
 
 group = "ojaynico.kotlin.react.native"
 version = "1.0"
+
+val artifactName = project.name
+val artifactGroup = project.group.toString()
+val artifactVersion = project.version.toString()
+
+val pomUrl = "https://github.com/ojaynico/ojaynico-kotlin-react-native"
+val pomScmUrl = "https://github.com/ojaynico/ojaynico-kotlin-react-native"
+val pomIssueUrl = "https://github.com/ojaynico/ojaynico-kotlin-react-native/issues"
+val pomDesc = "https://github.com/ojaynico/ojaynico-kotlin-react-native"
+
+val githubRepo = "https://github.com/ojaynico"
+val githubReadme = "README.md"
+
+val pomLicenseName = "MIT"
+val pomLicenseUrl = "https://opensource.org/licenses/mit-license.php"
+val pomLicenseDist = "repo"
+
+val pomDeveloperId = "ojaynico"
+val pomDeveloperName = "Nicodemus Ojwee"
 
 repositories {
     maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
@@ -13,54 +33,83 @@ repositories {
     maven { url = uri("http://dl.bintray.com/kotlin/kotlin-js-wrappers") }
     maven { url = uri("https://dl.bintray.com/kotlin/kotlinx") }
 }
-kotlin {
-    jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
+
+/*val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.getByName("main").allSource)
+}*/
+
+publishing {
+    publications {
+        create<MavenPublication>("ojaynico-kotlin-react-native") {
+            groupId = artifactGroup
+            artifactId = artifactName
+            version = artifactVersion
+            //from(components["java"])
+
+            //artifact(sourcesJar)
+
+            pom.withXml {
+                asNode().apply {
+                    appendNode("description", pomDesc)
+                    appendNode("name", rootProject.name)
+                    appendNode("url", pomUrl)
+                    appendNode("licenses").appendNode("license").apply {
+                        appendNode("name", pomLicenseName)
+                        appendNode("url", pomLicenseUrl)
+                        appendNode("distribution", pomLicenseDist)
+                    }
+                    appendNode("developers").appendNode("developer").apply {
+                        appendNode("id", pomDeveloperId)
+                        appendNode("name", pomDeveloperName)
+                    }
+                    appendNode("scm").apply {
+                        appendNode("url", pomScmUrl)
+                    }
+                }
+            }
         }
     }
+}
+
+bintray {
+    user = project.findProperty("bintrayUser").toString()
+    key = project.findProperty("bintrayKey").toString()
+    publish = true
+
+    setPublications("ojaynico-kotlin-react-native")
+
+    pkg.apply {
+        repo = "ojaynico-kotlin-react-native"
+        name = artifactName
+        githubRepo = githubRepo
+        vcsUrl = pomScmUrl
+        description = "Kotlin wrapper for react native components and APIs"
+        setLabels("kotlin", "react", "react-native")
+        setLicenses("MIT")
+        desc = description
+        websiteUrl = pomUrl
+        issueTrackerUrl = pomIssueUrl
+        githubReleaseNotesFile = githubReadme
+
+        version.apply {
+            name = artifactVersion
+            desc = pomDesc
+            released = "2020-11-17"
+            vcsTag = artifactVersion
+        }
+    }
+}
+
+kotlin {
     js {
         nodejs()
         useCommonJs()
     }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
-
-
-    sourceSets {
-        val commonMain by getting
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-            }
-        }
-        val jvmMain by getting
-        val jvmTest by getting {
-            dependencies {
-                implementation(kotlin("test-junit"))
-            }
-        }
-        val jsMain by getting {
-            dependencies {
-                implementation("org.jetbrains:kotlin-react:16.13.1-pre.120-kotlin-1.4.10")
-                implementation("org.jetbrains:kotlin-extensions:1.0.1-pre.120-kotlin-1.4.10")
-                implementation(npm("react", "16.13.1"))
-                implementation(npm("react-native", "0.63.3", generateExternals=true))
-            }
-        }
-        val jsTest by getting {
-            dependencies {
-                implementation(kotlin("test-js"))
-            }
-        }
-        val nativeMain by getting
-        val nativeTest by getting
+    dependencies {
+        implementation("org.jetbrains:kotlin-react:16.13.1-pre.120-kotlin-1.4.10")
+        implementation("org.jetbrains:kotlin-extensions:1.0.1-pre.120-kotlin-1.4.10")
+        implementation(npm("react", "16.13.1"))
+        implementation(npm("react-native", "0.63.3", generateExternals=true))
     }
 }
